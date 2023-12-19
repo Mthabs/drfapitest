ab33d414de31286e97023df70005a8e2de488b68
Mthabs <>
2023-12-19 19:45:21 +0000

    add changes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -73,21 +73,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'rest_framework.authtoken', 
+    'rest_framework',
+    'django_filters',
+    'rest_framework.authtoken',
     'dj_rest_auth',
-    'django.contrib.sites', 
-    'allauth', 
-    'allauth.account', 
-    'allauth.socialaccount', 
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'corsheaders',
-
 
     'profiles',
-
+    'posts',
+    'comments',
+    'likes',
+    'followers',
 ]
 SITE_ID = 1
 MIDDLEWARE = [
