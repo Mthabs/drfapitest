9ed6f62269d4d665f01a5c3b2acfdc722b0e0970
Mthabs <>
2023-12-20 10:47:41 +0000

    add changes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -108,7 +108,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        r"^https://3000-mthabs-mometest-rus5z60391\.us2\.codeanyapp\.com$",
     ]
 CORS_ALLOW_CREDENTIALS = True
 
