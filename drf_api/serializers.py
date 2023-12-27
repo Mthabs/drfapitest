8196e83477e89c6c275123a288c8527f61749913
Mthabs <>
2023-12-27 09:49:40 +0000

    add changes to the mainserializers file

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,12 +1,14 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
-
+from chatcom.friends_chats.serializers import UserProfileSerializer 
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='profile.profile_picture.url') 
+
+    profile = UserProfileSerializer()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'profile'
         )
